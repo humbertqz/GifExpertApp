ad49e782e68ddb2795316a43aac4ece80a28fccb
humberto quezada <humberto.shot@gmail.com>
2020-08-21 11:14:58 -0500

    docs agregado

---
+++ docs/precache-manifest.175f0ca8ccfcd3ef294ed51829463f68.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "371f8ff95c6a2583ac6e4de40b0daf79",
+    "url": "/index.html"
+  },
+  {
+    "revision": "99eada8dc82838c9da31",
+    "url": "/static/css/main.cb9e59c0.chunk.css"
+  },
+  {
+    "revision": "100caeaeffd01e7cd693",
+    "url": "/static/js/2.78557d1b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.78557d1b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "99eada8dc82838c9da31",
+    "url": "/static/js/main.95259fab.chunk.js"
+  },
+  {
+    "revision": "e5ee6826b74226f47af5",
+    "url": "/static/js/runtime-main.b577d66e.js"
+  }
+]);
